7e22c3b31db1988f458cd71c1980d69d6c3281a5
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-11 22:01:45 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecrowd_Consumption_1014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X;
+            double Y, avgDistance;
+            X = Convert.ToInt32(Console.ReadLine());
+            Y = Convert.ToDouble(Console.ReadLine());
+
+            avgDistance = (X/Y);
+            Console.WriteLine("Average Distance = {0:00.00}", avgDistance);
+            Console.ReadKey();
+        }
+    }
+}
